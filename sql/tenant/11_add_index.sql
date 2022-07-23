b7d42c1fa3bab0e32b53704ad637d29c26ac90e7
sh4869 <nobuk4869@gmail.com>
2022-07-23 15:50:37 +0900

    fix sql

--- sql/tenant/11_add_index.sql
+++ sql/tenant/11_add_index.sql
@@ -1,3 +1,3 @@
 CREATE INDEX player_tenant_id_created_at_index on player(tenant_id, created_at DESC);
 CREATE INDEX player_score_ranking_index on player_score(tenant_id, competition_id, row_num DESC);
-CREATE INDEX player_score_individual_index on player_score(tenant_id, competition_id, player_id, row_num DESC);
+CREATE INDEX player_score_individual_index on player_score(player_id, tenant_id,competition_id, row_num DESC);
