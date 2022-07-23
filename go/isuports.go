03c2dac90eef160090fddb1cbff6320125cdfb61
sh4869 <nobuk4869@gmail.com>
2022-07-23 15:35:31 +0900

    rankを使ってみる

--- go/isuports.go
+++ go/isuports.go
@@ -1234,25 +1234,12 @@ func playerHandler(c echo.Context) error {
 		return fmt.Errorf("error flockByTenantID: %w", err)
 	}
 	defer fl.Close()
+
 	pss := make([]PlayerScoreRow, 0, len(cs))
-	for _, c := range cs {
-		ps := PlayerScoreRow{}
-		if err := tenantDB.GetContext(
-			ctx,
-			&ps,
-			// 最後にCSVに登場したスコアを採用する = row_numが一番大きいもの
-			"SELECT * FROM player_score WHERE tenant_id = ? AND competition_id = ? AND player_id = ? ORDER BY row_num DESC LIMIT 1",
-			v.tenantID,
-			c.ID,
-			p.ID,
-		); err != nil {
-			// 行がない = スコアが記録されてない
-			if errors.Is(err, sql.ErrNoRows) {
-				continue
-			}
-			return fmt.Errorf("error Select player_score: tenantID=%d, competitionID=%s, playerID=%s, %w", v.tenantID, c.ID, p.ID, err)
+	if err := tenantDB.SelectContext(ctx, &pss, "SELECT * FROM (SELECT *, RANK () OVER (PARTITION BY competition_id  ORDER BY row_num DESC) AS rank FROM player_score WHERE tenant_id = ? AND player_id = ?) WHERE rank = 1", v.tenantID, p.ID); err != nil {
+		if errors.Is(err, sql.ErrNoRows) {
+			return fmt.Errorf("error Select player_score: tenantID=%d, competitionID=%s, playerID=%s, %w", v.tenantID, cs[0].ID, p.ID, err)
 		}
-		pss = append(pss, ps)
 	}
 
 	psds := make([]PlayerScoreDetail, 0, len(pss))
