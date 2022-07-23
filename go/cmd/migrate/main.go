c7e82c588c4433777d904a46c568579ffe2e5e8d
isu3 <isu3>
2022-07-23 06:50:11 +0000

    [ito] sqlite をmysqlに移行するコード

--- go/cmd/migrate/main.go
+++ go/cmd/migrate/main.go
@@ -1,4 +1,4 @@
-package migrate
+package main
 
 import (
 	"fmt"
@@ -6,8 +6,14 @@ import (
 	"path/filepath"
 	"regexp"
 
+	"github.com/go-sql-driver/mysql"
 	"github.com/jmoiron/sqlx"
-)
+	_ "github.com/mattn/go-sqlite3"
+	"database/sql"
+
+	// "github.com/mattn/go-sqlite3"
+	// proxy "github.com/shogo82148/go-sql-proxy"
+) 
 
 var (
 	// 正しいテナント名の正規表現
@@ -47,6 +53,7 @@ func tenantDBPath(id int64) string {
 // テナントDBに接続する
 func connectToTenantDB(id int64) (*sqlx.DB, error) {
 	p := tenantDBPath(id)
+
 	db, err := sqlx.Open(sqliteDriverName, fmt.Sprintf("file:%s?mode=rw", p))
 	if err != nil {
 		return nil, fmt.Errorf("failed to open tenant DB: %w", err)
@@ -54,14 +61,121 @@ func connectToTenantDB(id int64) (*sqlx.DB, error) {
 	return db, nil
 }
 
+// 管理用DBに接続する
+func connectAdminDB() (*sqlx.DB, error) {
+	config := mysql.NewConfig()
+	config.Net = "tcp"
+	config.Addr = getEnv("ISUCON_DB_HOST", "127.0.0.1") + ":" + getEnv("ISUCON_DB_PORT", "3306")
+	config.User = getEnv("ISUCON_DB_USER", "isucon")
+	config.Passwd = getEnv("ISUCON_DB_PASSWORD", "isucon")
+	config.DBName = getEnv("ISUCON_DB_NAME", "isuports")
+	config.ParseTime = true
+	config.InterpolateParams = true
+	dsn := config.FormatDSN()
+	return sqlx.Open("mysql", dsn)
+}
+
+type PlayerRow struct {
+	TenantID       int64  `db:"tenant_id"`
+	ID             string `db:"id"`
+	DisplayName    string `db:"display_name"`
+	IsDisqualified bool   `db:"is_disqualified"`
+	CreatedAt      int64  `db:"created_at"`
+	UpdatedAt      int64  `db:"updated_at"`
+}
+
+type CompetitionRow struct {
+	TenantID   int64         `db:"tenant_id"`
+	ID         string        `db:"id"`
+	Title      string        `db:"title"`
+	FinishedAt sql.NullInt64 `db:"finished_at"`
+	CreatedAt  int64         `db:"created_at"`
+	UpdatedAt  int64         `db:"updated_at"`
+}
+
+type PlayerScoreRow struct {
+	TenantID      int64  `db:"tenant_id"`
+	ID            string `db:"id"`
+	PlayerID      string `db:"player_id"`
+	CompetitionID string `db:"competition_id"`
+	Score         int64  `db:"score"`
+	RowNum        int64  `db:"row_num"`
+	CreatedAt     int64  `db:"created_at"`
+	UpdatedAt     int64  `db:"updated_at"`
+}
+
 func main() {
-	for i := 0; i <= 100; i++ {
-		db, err := connectToTenantDB(int64(i))
+	num := 100
+	for i := 0; i <= num; i++ {
+		tenantDB, err := connectToTenantDB(int64(i))
 		if err != nil {
 			fmt.Println(err)
 			continue
 		}
-		defer db.Close()
-		fmt.Println(db.DriverName())
+		defer tenantDB.Close()
+
+		// players
+		var players []PlayerRow
+		if err = tenantDB.Select(
+		&players,
+		"SELECT * FROM player WHERE tenant_id=?",
+		int64(i),
+		); err != nil {
+			fmt.Errorf("error Select player: %w", err)
+		}
+		if players != nil {
+			if _, err := tenantDB.NamedExec(
+				"INSERT INTO player (id, tenant_id, display_name, is_disqualified, created_at, updated_at) VALUES (:id, :tenant_id, :display_name, :is_disqualified, :created_at, :updated_at)",
+				players,
+			); err != nil {
+				fmt.Errorf(
+					"error Insert player at tenantDB: id=%s, displayName=%s, isDisqualified=%t, createdAt=%d, updatedAt=%d, %w",
+					players[0].ID, players[0].DisplayName, false, players[0].CreatedAt, players[0].UpdatedAt, err,
+				)
+			}
+		}
+
+
+		// competition
+		var competitions []CompetitionRow
+		if err = tenantDB.Select(
+		&competitions,
+		"SELECT * FROM competition WHERE tenant_id=?",
+		int64(i),
+		); err != nil {
+			fmt.Errorf("error Select player: %w", err)
+		}
+
+		if competitions != nil {
+			if _, err := tenantDB.NamedExec(
+				"INSERT INTO competition (id, tenant_id, title, finished_at, created_at, updated_at) VALUES (:id, :tenant_id, :title, :finished_at, :created_at, :updated_at)",
+				competitions,
+			); err != nil {
+				fmt.Errorf(
+					"error Insert player at tenantDB: id=%s, createdAt=%d, updatedAt=%d, %w",
+					competitions[0].ID, competitions[0].CreatedAt, competitions[0].UpdatedAt, err,
+				)
+			}
+		}
+
+		// player_score
+		var playerScores []PlayerScoreRow
+		if err = tenantDB.Select(
+		&playerScores,
+		"SELECT * FROM player_score WHERE tenant_id=?",
+		int64(i),
+		); err != nil {
+			fmt.Errorf("error Select player: %w", err)
+		}
+
+		if playerScores != nil {
+			if _, err := tenantDB.NamedExec(
+				"INSERT INTO player_score (id, tenant_id, player_id, competition_id, score, row_num ,created_at, updated_at) VALUES (:id, :tenant_id, :player_id, :completition_id, :score, :row_num, :created_at, :updated_at)",
+				playerScores,
+			); err != nil {
+				fmt.Errorf("error Insert player_score")
+			}
+		}
+		fmt.Println(i)
 	}
 }
